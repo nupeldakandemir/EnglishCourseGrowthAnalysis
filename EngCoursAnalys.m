4df5053d8c8d147aed9e81eb9c62126f6a02c0bc
Nupelda Kandemir <sherlock2934@gmail.com>
2024-11-14 01:55:35 +0300

    Update EngCoursAnalys.m

--- EngCoursAnalys.m
+++ EngCoursAnalys.m
@@ -7,16 +7,16 @@ record_Num = [15, 20, 30, 45, 65, 90, 120, 155];
 %**Draw the data plot**%
 scatter(years, record_Num, 100, 'm', 's', 'filled'); %**'ms' for square **symbols
 
-set(gca, 'LineWidth', 4);   %**I set the the with**%
+set(gca, 'LineWidth', 4);   %**Set the the with**%
 
-xlim([0.5, 10]); %***I set x axis limit**%
+xlim([0.5, 10]); %*** Set x axis limit**%
 
 %**It is for label names of graph**%
 xlabel('Year');
 ylabel('Number of Student');
 title('Growth of Class Size in English Course Over 8 Years');
 
-%**I use grid it is optional if you do not want use grid off**%
+%**Use grid it is optional if you do not want use grid off**%
 grid on;
 
 %**It is about interpretation of the graph**%
